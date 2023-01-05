10f7d683268d4a960a7f9231b49fe46855578d64
akaprasanga <akaprasanganeupane@gmail.com>
2023-01-05 18:57:29 +0000

    first changes 3 more figures

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,52 @@ my_dataframe = pd.read_csv("CO2 Emissions_Canada.csv")
 st.header('Co2 emissions per brand')
 Brand_data = my_dataframe.groupby(['Make']).mean()
 fig = px.bar(Brand_data, x = Brand_data.index,  y='CO2 Emissions(g/km)', title = "Mean Co2 Emissions by Brand (g/km)", color = "CO2 Emissions(g/km)")
+fig.update_traces(line_color='red')
 fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
 st.plotly_chart(fig)
 st.markdown("Conclusion of the figure")
 st.markdown("""---""")
+
+# Graph 2: Mean CO2 Emissions per Vehicle Class 
+st.header('Mean CO2 Emissions by Vehicle Class')
+means = my_dataframe.groupby("Vehicle Class").mean()
+vclass = px.bar(means, y = "CO2 Emissions(g/km)", title = "Mean Emissions per Vehicle Class", color = "CO2 Emissions(g/km)")
+vclass.update_layout(barmode='stack', xaxis={'categoryorder':'total ascending'})
+vclass.update_traces(line_color='green')
+st.plotly_chart(vclass)
+st.markdown('Conclusion: Passenger and Cargo Vans have the highest enviornmental impact in terms of CO2 emmissions, while Stantion Wagons and Compact vehicles have the lowest impact.')
+st.markdown("""---""")
+
+# Graph 3:
+st.header('Fuel type vs carbon emissions ')
+Fuel_Data = my_dataframe
+Fuel_data = Fuel_Data.rename(index={'D':"Diesel", 'E':"Ethanol (E85)","N":"Natural Gas","X":"Regular","Z":"Premium"})
+fig = px.box(my_dataframe, x="Fuel Type", y="CO2 Emissions(g/km)")
+st.plotly_chart(fig)
+st.markdown("Conclusion")
+st.markdown("""---""")
+
+
+
+
+
+#Graph 4:
+
+
+
+
+#Graph 5:
+Fuel_Data = my_dataframe.groupby(['Fuel Type']).mean()
+Fuel_data = Fuel_Data.rename(index={'D':"Diesel", 'E':"Ethanol (E85)","N":"Natural Gas","X":"Regular","Z":"Premium"})
+st.write(Fuel_data)
+
+fig = px.pie(Fuel_data, values='Fuel Consumption Comb (L/100 km)', names = Fuel_data.index, title = "Fuel Type's Effect on Fuel Consumption (Combined)" )
+st.plotly_chart(fig)
+st.markdown("Conclusion: Diesel is the most efficient of the fuels provided, with ethanol being the least efficient. Regular is more efficient than premium.")
+st.markdown("""---""")
+
+# Graph 6:
+
 
 # #show off a bit of your data. 
 # st.header('The Data')
